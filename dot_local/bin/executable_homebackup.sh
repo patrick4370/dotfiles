214f1fb725305522f189fdedcab0adb8add6fe1a
Patrick Heffernan <patrick4370@bigpond.com>
2023-11-01 14:18:05 +1000

    Adjusted homebackup.sh

--- dot_local/bin/executable_homebackup.sh
+++ dot_local/bin/executable_homebackup.sh
@@ -14,7 +14,7 @@
 pushd $HOME
 
 if grep -sq '/run/media/patrick/D923-4A19' /proc/mounts; then
-    rsync -zhavp --delete --safe-links --files-from=home.txt --exclude=".*" . /run/media/patrick/D923-4A19/Dell
+    rsync -zhavp --delete --safe-links --files-from=home.txt . /run/media/patrick/D923-4A19/Dell
     dunstify "Success" "Backup completed at $(date +"%d/%m/%Y %H:%M")" -u critical
 else
     dunstify "Failure" "/run/media/patrick/D923-4A19 is not mounted" -u critical
